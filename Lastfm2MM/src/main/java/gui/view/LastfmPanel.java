98ef762330eebbfebf59f50aa65afdeef76bed6d
Saeger <Saeger>
2012-11-16 23:08:33 +0100

    change packagestructure

--- Lastfm2MM/src/main/java/gui/view/LastfmPanel.java
+++ Lastfm2MM/src/main/java/gui/view/LastfmPanel.java
@@ -1,4 +1,4 @@
-package gui;
+package gui.view;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
--- Lastfm2MM/src/main/java/gui/view/MainFrame.java
+++ Lastfm2MM/src/main/java/gui/view/MainFrame.java
@@ -1,4 +1,5 @@
-package gui;
+package gui.view;
+
 
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
@@ -40,6 +41,7 @@ public class MainFrame extends JFrame {
 	 * Create the frame.
 	 */
 	public MainFrame() {
+		setTitle("Lastfm2MM");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 800, 600);
 		contentPane = new JPanel();
