204179df9c7d984cd722180dfab5403f1f84304d
Saeger <Saeger>
2012-11-16 20:32:38 +0100

    drop old MainFrame

--- Lastfm2MM/src/main/java/gui/MainFrame2.java
+++ Lastfm2MM/src/main/java/gui/MainFrame2.java
@@ -1,14 +1,10 @@
 package gui;
 import java.awt.EventQueue;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.JScrollPane;
 import javax.swing.JTabbedPane;
-import javax.swing.JTable;
 import javax.swing.UIManager;
 import javax.swing.border.EmptyBorder;
 
@@ -16,10 +12,9 @@ import com.jgoodies.forms.factories.FormFactory;
 import com.jgoodies.forms.layout.ColumnSpec;
 import com.jgoodies.forms.layout.FormLayout;
 import com.jgoodies.forms.layout.RowSpec;
-import javax.swing.table.DefaultTableModel;
 
 
-public class MainFrame extends JFrame {
+public class MainFrame2 extends JFrame {
 
 	private JPanel contentPane;
 
@@ -31,7 +26,7 @@ public class MainFrame extends JFrame {
 			public void run() {
 				try {
 					UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-					MainFrame frame = new MainFrame();
+					MainFrame2 frame = new MainFrame2();
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -43,7 +38,7 @@ public class MainFrame extends JFrame {
 	/**
 	 * Create the frame.
 	 */
-	public MainFrame() {
+	public MainFrame2() {
 		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 580, 400);
