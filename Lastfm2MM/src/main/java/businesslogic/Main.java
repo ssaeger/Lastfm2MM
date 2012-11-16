d7f49a3a002d0304718c4521775d8cb335a2b86d
Saeger <Saeger@mail.de>
2012-11-16 23:45:53 +0100

    new programmentry point

---
+++ Lastfm2MM/src/main/java/businesslogic/Main.java
@@ -0,0 +1,10 @@
+package businesslogic;
+
+import businesslogic.controller.MainFrameController;
+
+public class Main {
+	public static void main(String[] args) {
+		MainFrameController mainFrameController = new MainFrameController();
+		mainFrameController.showView();
+	}
+}
