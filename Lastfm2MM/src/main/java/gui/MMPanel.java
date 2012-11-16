d0930fd96cdedc1ca4b9f1379574a22c38a17ffa
Saeger <Saeger>
2012-11-16 21:44:56 +0100

    MMpanel has now radio buttons with functionality

--- Lastfm2MM/src/main/java/gui/MMPanel.java
+++ Lastfm2MM/src/main/java/gui/MMPanel.java
@@ -1,21 +1,25 @@
 package gui;
 
-import javax.swing.JPanel;
-import com.jgoodies.forms.layout.FormLayout;
-import com.jgoodies.forms.layout.ColumnSpec;
-import com.jgoodies.forms.layout.RowSpec;
-import com.jgoodies.forms.factories.FormFactory;
-
-import javax.swing.JFileChooser;
-import javax.swing.JTextField;
-import javax.swing.JButton;
-import javax.swing.JLabel;
-import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
+
+import javax.swing.ButtonGroup;
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+import com.jgoodies.forms.factories.FormFactory;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.RowSpec;
+import javax.swing.JRadioButton;
 
 public class MMPanel extends JPanel {
 	private JTextField textField;
+	private JButton btnSelectDatabase;
 
 	/**
 	 * Create the panel.
@@ -34,16 +38,41 @@ public class MMPanel extends JPanel {
 				FormFactory.RELATED_GAP_ROWSPEC,
 				FormFactory.DEFAULT_ROWSPEC,
 				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
 				FormFactory.DEFAULT_ROWSPEC,}));
 		
-		JLabel lblDatabase = new JLabel("Database:");
-		add(lblDatabase, "2, 4, right, default");
+		JRadioButton rdbtnNewData = new JRadioButton("New Data");
+		rdbtnNewData.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				textField.setEnabled(false);
+				btnSelectDatabase.setEnabled(false);
+			}
+		});
+		rdbtnNewData.setSelected(true);
+		add(rdbtnNewData, "2, 2");
+		
+		JRadioButton rdbtnUpdateData = new JRadioButton("Update Data");
+		rdbtnUpdateData.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				textField.setEnabled(true);
+				btnSelectDatabase.setEnabled(true);
+			}
+		});
+		add(rdbtnUpdateData, "2, 4");
+		
+		ButtonGroup btnGroup = new ButtonGroup();
+		btnGroup.add(rdbtnNewData);
+		btnGroup.add(rdbtnUpdateData);
 		
 		textField = new JTextField();
+		textField.setEnabled(false);
 		add(textField, "4, 4, fill, default");
 		textField.setColumns(10);
 		
-		JButton btnSelectDatabase = new JButton("Select Database");
+		btnSelectDatabase = new JButton("Select Data");
+		btnSelectDatabase.setEnabled(false);
+		btnSelectDatabase.setActionCommand("Select Data");
 		btnSelectDatabase.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				
@@ -52,11 +81,11 @@ public class MMPanel extends JPanel {
 
 		        if (returnVal == JFileChooser.APPROVE_OPTION) {
 		            File file = fc.getSelectedFile();
+		            textField.setText(file.getAbsolutePath());
 		        }
 			}
 		});
 		add(btnSelectDatabase, "6, 4");
 
 	}
-
 }
