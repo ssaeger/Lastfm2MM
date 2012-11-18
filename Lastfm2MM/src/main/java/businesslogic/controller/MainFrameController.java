273b80127d7572b7a80e4e2e38d74845062d6ae5
Saeger <Saeger@mail.de>
2012-11-18 18:55:03 +0100

    small changes to fit MVC

--- Lastfm2MM/src/main/java/businesslogic/controller/MainFrameController.java
+++ Lastfm2MM/src/main/java/businesslogic/controller/MainFrameController.java
@@ -1,25 +1,25 @@
 package businesslogic.controller;
 
 import gui.view.MainFrame;
-
-import java.awt.EventQueue;
-
 import javax.swing.UIManager;
 
 public class MainFrameController {
+	
+	private MainFrame mainFrame;
+	
+	public MainFrameController() {
+		showView();
+		MMPanelController mmPanelController = new MMPanelController(mainFrame.getMmPanel());
+	}
 
 	public void showView() {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					UIManager.setLookAndFeel(UIManager
-							.getSystemLookAndFeelClassName());
-					MainFrame frame = new MainFrame();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
+		try {
+			UIManager.setLookAndFeel(UIManager
+					.getSystemLookAndFeelClassName());
+			mainFrame = new MainFrame();
+			mainFrame.setVisible(true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
--- Lastfm2MM/src/main/java/gui/view/MainFrame.java
+++ Lastfm2MM/src/main/java/gui/view/MainFrame.java
@@ -1,6 +1,7 @@
 package gui.view;
 
 import java.awt.Color;
+import java.awt.HeadlessException;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -73,4 +74,77 @@ public class MainFrame extends JFrame {
 		contentPane.add(controlPanel, "6, 8, fill, fill");
 	}
 
+	public MainFrame(LastfmPanel lastfmPanel, MMPanel mmPanel,
+			DataPanel dataPanel, JPanel statusPanel, ControlPanel controlPanel)
+			throws HeadlessException {
+		setTitle("Lastfm2MM");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 800, 600);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(new FormLayout(new ColumnSpec[] {
+				FormFactory.RELATED_GAP_COLSPEC,
+				FormFactory.DEFAULT_COLSPEC,
+				FormFactory.RELATED_GAP_COLSPEC,
+				FormFactory.GROWING_BUTTON_COLSPEC,
+				FormFactory.RELATED_GAP_COLSPEC,
+				ColumnSpec.decode("default:grow"),
+				FormFactory.RELATED_GAP_COLSPEC,
+				FormFactory.DEFAULT_COLSPEC,},
+			new RowSpec[] {
+				FormFactory.RELATED_GAP_ROWSPEC,
+				RowSpec.decode("140dlu"),
+				FormFactory.RELATED_GAP_ROWSPEC,
+				RowSpec.decode("top:default:grow"),
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,}));
+		
+		this.lastfmPanel = lastfmPanel;
+		this.mmPanel = mmPanel;
+		this.dataPanel = dataPanel;
+		this.statusPanel = statusPanel;
+		this.controlPanel = controlPanel;
+		insertPanels();
+	}
+	
+	public LastfmPanel getLastfmPanel() {
+		return lastfmPanel;
+	}
+
+	public MMPanel getMmPanel() {
+		return mmPanel;
+	}
+
+	public DataPanel getDataPanel() {
+		return dataPanel;
+	}
+
+	public JPanel getStatusPanel() {
+		return statusPanel;
+	}
+
+	public ControlPanel getControlPanel() {
+		return controlPanel;
+	}
+
+	private void insertPanels() {
+		lastfmPanel.setBorder(new TitledBorder(new LineBorder(
+				new Color(0, 0, 0)), "Last.fm Settings", TitledBorder.LEFT,
+				TitledBorder.TOP, null, new Color(0, 0, 0)));
+		contentPane.add(lastfmPanel, "4, 2, fill, fill");
+
+		mmPanel.setBorder(new TitledBorder(new LineBorder(new Color(0, 0, 0)),
+				"MediaMonkey Settings", TitledBorder.LEADING, TitledBorder.TOP,
+				null, null));
+		contentPane.add(mmPanel, "6, 2, fill, fill");
+
+		contentPane.add(dataPanel, "4, 4, 3, 1, fill, fill");
+
+		contentPane.add(statusPanel, "4, 8, fill, fill");
+
+		contentPane.add(controlPanel, "6, 8, fill, fill");
+	}
 }
